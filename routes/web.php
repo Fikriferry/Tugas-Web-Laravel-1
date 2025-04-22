711fd9233ee4542b66e1036343a68cf18b0f16be
Fikriferry <fikriferryf1@gmail.com>
2025-04-22 12:35:32 +0700

    [Database - Menambahkan tabel Products]

--- routes/web.php
+++ routes/web.php
@@ -2,38 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
+use App\Http\Controllers\HomepageController;
+
+use Illuminate\Http\Request;
 
 Route::get('/p', function () {
     return view('welcome');
 })->name('home');
 
-Route::get('/', function () {
-    return view('web.homepage');
-});
+Route::get('/',[HomepageController::class,'index']);
+Route::get('products', [HomepageController::class, 'products']);
+Route::get('product/{slug}', [HomepageController::class, 'product']);
+Route::get('categories',[HomepageController::class, 'categories']);
+Route::get('category/{slug}', [HomepageController::class, 'category']);
+Route::get('cart', [HomepageController::class, 'cart']);
+Route::get('checkout', [HomepageController::class, 'checkout']);
 
-Route::get('/products', function () {
-    return view('web.products');
-});
-
-Route::get('products/{slug}', function ($slug) {
-    return view('web.single_products');
-});
-
-Route::get('/categories', function () {
-    return view('web.categories');
-});
-
-Route::get('categories/{slug}', function ($slug) {
-    return view('web.single_categories');
-});
-
-Route::get('/cart', function () {
-    return view('web.cart');
-});
-
-Route::get('/chekout', function () {
-    return view('web.checkout');
-});
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
